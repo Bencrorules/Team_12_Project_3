ae186b71de6a966e26bf2771fceed157bb4be5c8
Sam Aldeguer <aldeguersam@gmail.com>
2022-03-31 16:28:46 -0600

    Adds image on interface, needs to be scaled

--- interface.py
+++ interface.py
@@ -5,23 +5,29 @@ from tkinter import filedialog
 
 root = tk.Tk()
 root.title("B/W to RGB Image Converter")
+root.attributes('-fullscreen', True)
 
-canvas = Canvas(height="700", width="1000")
 
-canvas.create_text(500, 30, text="Black and White to Colored Image Converter", font=('Helvetica', 30, 'bold'))
-canvas.create_text(500, 60, text="Project 3 by Team 12: Sam Aldeguer, Ben Crocker, Peter Lee, Jordan Cedeno", font=('Helvetica', 15))
+canvas = Canvas(height="10000", width="10000")
+
+canvas.create_text(700, 30, text="Black and White to Colored Image Converter", font=('Helvetica', 30, 'bold'))
+canvas.create_text(700, 60, text="Project 3 by Team 12: Sam Aldeguer, Ben Crocker, Peter Lee, Jordan Cedeno", font=('Helvetica', 15))
 
 def upload():
     global img
-    root.filename = filedialog.askopenfilename(initialdir="/", title="Select a file", filetypes=(("jpg files", "*.jpg"),("all files", "*.*")))
-    file_label = Label(root, text=root.filename).pack()
+    global new_image
+    global imageHeight
+    global imageWidth
+    root.filename = filedialog.askopenfilename(initialdir="/", title="Select a file", filetypes=(("jpg files", "*.jpg"),("png files", "*.png"),("all files", "*.*")))
     img = Image.open(root.filename)
-    img = img.resize((200,200))
-    img = ImageTk.PhotoImage(img)
-    img_label = Label(image=img).pack()
+    new_image = ImageTk.PhotoImage(img)
+    imageHeight = new_image.height()
+    imageWidth = new_image.width()
+    
+    canvas.create_image(1,300,anchor=NW, image=new_image)
 
 uploadButton = Button(root, text="Upload image", width=18, height=5, bd='10', command=upload)
-uploadButton.place(x=415, y=75)
+uploadButton.place(x=600, y=75)
 
 
 canvas.pack()
