86a684f1774806ed658e2ecd7c8eb2c15e4afda0
Jordan <jcedeno870@gmail.com>
2022-04-01 14:51:00 -0500

    Fixed scaling issue with title and button

--- interface.py
+++ interface.py
@@ -10,8 +10,10 @@ root.attributes('-fullscreen', True)
 canvas = Canvas(height="10000", width="10000")
 
 #Adds title to the top of the GUI
-canvas.create_text(715, 30, text="Black & White to Colored Image Converter", font=('Helvetica', 30, 'bold'))
-canvas.create_text(715, 60, text="Project 3 by Team 12: Sam Aldeguer, Ben Crocker, Peter Lee, Jordan Cedeno", font=('Helvetica', 15))
+title = Label(root, text="Black & White to Colored Image Converter", font=('Helvetica', 30, 'bold'))
+title.pack()
+names = Label(root, anchor=CENTER, text="Project 3 by Team 12: Sam Aldeguer, Ben Crocker, Peter Lee, Jordan Cedeno", font=('Helvetica', 15))
+names.pack()
 
 #Method that uploads the image from the button and places the images.
 def upload():
@@ -27,7 +29,8 @@ def upload():
     canvas.create_image(150,300,anchor=NW, image=new_image) #adds image to GUI
 
 uploadButton = Button(root, text="Upload image", width=18, height=5, bd='10', command=upload) #creates upload button
-uploadButton.place(x=610, y=75) #places upload button at top center of screen
+
+uploadButton.place(relx=0.5, rely=0.15, anchor=CENTER) #places upload button at top center of screen
 
 canvas.create_text(400, 250, text="Black & White Photo", font=('Helvetica', 25, 'bold')); #Black & White photo label
 canvas.create_text(1050, 250, text="Colored", font=('Helvetica', 25, 'bold')); #Colored photo label
