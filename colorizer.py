11da0aea79673546baae350c3a2228fa49f49b4a
Bencrorules <bencrorules@gmail.com>
2022-04-22 19:04:38 -0500

    Connected UI and Backend, Added Color

---
+++ colorizer.py
@@ -0,0 +1,57 @@
+import numpy as np
+import cv2
+
+# ~ Credits ~
+# Models: https://github.com/richzhang/colorization/tree/caffe/colorization/models
+# Points: https://github.com/richzhang/colorization/blob/caffe/colorization/resources/pts_in_hull.npy
+
+def colorize(image):
+
+    # sets the files in root to some variable names
+    modelPath = 'colorization_release_v2.caffemodel'
+    protoPath = 'colorization_deploy_v2.prototxt'
+    kernelPath = 'pts_in_hull.npy'
+    imagePath = image
+
+    # calls a function within the pre-trained neural network using our various imported files
+    neuralNetwork = cv2.dnn.readNetFromCaffe(protoPath, modelPath)
+
+    # the following few lines are copied/slightly edited from OpenCV Documentation of kernel manipulation
+    points = np.load(kernelPath)
+    # defines the size of the point/kernel to be 1x1
+    points = points.transpose().reshape(2, 313, 1, 1)
+    # obtains the type of color restoration we'll be using. in this case it'll be LAB instead of your typical RGB
+    neuralNetwork.getLayer(neuralNetwork.getLayerId("class8_ab")).blobs = [points.astype(np.float32)]
+    neuralNetwork.getLayer(neuralNetwork.getLayerId("conv8_313_rh")).blobs = [np.full([1, 313], 2.606, dtype="float32")]
+
+    # original code - image manipulation to allow neural network to comprehend
+    # loads our selected image into a variable through the OpenCV function imread
+    bwImage = cv2.imread(imagePath)
+    # normalizes the image to values between 0 and 1 instead of 0 to 255
+    normalizedImage = bwImage.astype("float32") / 255.0
+    # converts image data to the LAB formatting our imported neural network is trained on
+    labImageData = cv2.cvtColor(normalizedImage, cv2.COLOR_BGR2LAB)
+
+    # resizes the image data to fit the 224x224 canvas the imported neural network is trained on
+    resizedImage = cv2.resize(labImageData, (224, 224))
+    # sets new constant as a split from the image. reduced by the mean lightness level, can be played with for dif results
+    lightness = cv2.split(resizedImage)[0]
+    lightness -= 50
+
+    # imports lightness data into the neural network and receives data back stored into variable ab
+    neuralNetwork.setInput(cv2.dnn.blobFromImage(lightness))
+    ab = neuralNetwork.forward()[0,:,:,:].transpose((1,2,0))
+
+    # resizes the data back to the original pictures dimensions
+    ab = cv2.resize(ab, (bwImage.shape[1], bwImage.shape[0]))
+    lightness = cv2.split(labImageData)[0]
+
+    # converts our newly obtained ab data back into RGB for image display
+    colorizedImage = np.concatenate((lightness[:, :, np.newaxis], ab), axis=2)
+    colorizedImage = cv2.cvtColor(colorizedImage, cv2.COLOR_LAB2BGR)
+    colorizedImage = (255.0 * colorizedImage).astype("uint8")
+
+    # saves the newly created image
+    filename = 'finalImage.jpg'
+    cv2.imwrite(filename, colorizedImage)
+    return filename
--- interface.py
+++ interface.py
@@ -1,49 +1,64 @@
 import tkinter as tk
+import colorizer
 from tkinter import *
 from PIL import ImageTk, Image
 from tkinter import filedialog
-
 
 root = tk.Tk()
 root.attributes('-fullscreen', True)
 root.title("B/W to RGB Image Converter")
 
-canvas = Canvas(height="10000", width="10000")
+canvas = Canvas(height="10000", width="10000", bg="#476A6F")
 
-#Adds titles to the top of the GUI
-title = Label(root, text="Black & White to Colored Image Converter", font=('Helvetica', 30, 'bold'))
+# Adds titles to the top of the GUI
+title = Label(root, text="Black & White to Colored Image Converter", font=('Helvetica', 30, 'bold'), fg="#1F2633")
 title.pack()
-names = Label(root, anchor=CENTER, text="Project 3 by Team 12: Sam Aldeguer, Ben Crocker, Peter Lee, Jordan Cedeno", font=('Helvetica', 15))
+names = Label(root, anchor=CENTER, text="Project 3 by Team 12: Sam Aldeguer, Ben Crocker, Peter Lee, Jordan Cedeno",
+              font=('Helvetica', 15), fg="#1F2633")
 names.pack()
 
-#Method that uploads the image from the button and places the images.
+
+# Method that uploads the image from the button and places the images.
 def upload():
     global img
-    global new_image
+    global bw_image
+    global cl_image
     global resized_image
     global imageHeight
     global imageWidth
-    root.filename = filedialog.askopenfilename(initialdir="/", title="Select a file", filetypes=(("jpg files", "*.jpg"),("png files", "*.png"),("jpeg files", "*.jpeg"),("all files", "*.*"))) #opens file browser
-    img = Image.open(root.filename) #puts image path into variable
-    imageWidth,imageHeight = img.size #gets width and height of image
-    resized_image = img.resize((500, (int)((imageHeight/imageWidth)*500))) #scales the image to make it fit on interface
-    new_image = ImageTk.PhotoImage(resized_image) #turns image path into image
+    root.filename = filedialog.askopenfilename(initialdir="/", title="Select a file", filetypes=(
+    ("jpg files", "*.jpg"), ("png files", "*.png"), ("jpeg files", "*.jpeg"),
+    ("all files", "*.*")))  # opens file browser
+    print(root.filename)
+    img = Image.open(root.filename)  # puts image path into variable
+    colored_image_path = colorizer.colorize(root.filename)
+    colored_image = Image.open(colored_image_path)
 
-    imageLabel = Label(root, image=new_image)
-    imageLabel.place(relx=0.15, rely=0.6, anchor=W) #adds image to GUI
+    imageWidth, imageHeight = img.size  # gets width and height of image
+    resized_image = img.resize(
+        (800, int((imageHeight / imageWidth) * 800)))  # scales the image to make it fit on interface
+    bw_image = ImageTk.PhotoImage(resized_image)  # turns image path into image
 
-uploadButton = Button(root, text="Upload image", width=18, height=5, bd='10', command=upload) #creates upload button
+    imageLabel1 = Label(root, image=bw_image)
+    imageLabel1.place(relx=0.100, rely=0.6, anchor=W)  # adds image to GUI
+
+    imageWidth, imageHeight = colored_image.size  # gets width and height of image
+    resized_image = colored_image.resize(
+        (800, int((imageHeight / imageWidth) * 800)))  # scales the image to make it fit on interface
+    cl_image = ImageTk.PhotoImage(resized_image)  # turns image path into image
+
+    imageLabel2 = Label(root, image=cl_image)
+    imageLabel2.place(relx=0.90, rely=0.6, anchor=E)  # adds image to GUI
 
 
-uploadButton.place(relx=0.5, rely=0.15, anchor=CENTER) #places upload button at top center of screen
+uploadButton = Button(root, text="Upload image", width=18, height=5, bd='15', command=upload, bg="#e9f0ea", fg="#1F2633", font=('Helvetica', 16))  # creates upload button
+uploadButton.place(relx=0.5, rely=0.15, anchor=CENTER)  # places upload button at top center of screen
 
-bwLabel = Label(root, text="Black & White Photo", font=('Helvetica', 25, 'bold')) #Black & White photo label
-bwLabel.place(relx=0.2, rely=0.35, anchor=W)
-colorLabel = Label(root, text="Colored", font=('Helvetica', 25, 'bold')) #Colored photo label
-colorLabel.place(relx=0.8, rely=0.35, anchor=E)
+bwLabel = Label(root, text="Black & White Photo", font=('Helvetica', 32, 'bold'), bg="#476A6F", fg="#1F2633")  # Black & White photo label
+bwLabel.place(relx=0.175, rely=0.35, anchor=W)
+colorLabel = Label(root, text="Colored", font=('Helvetica', 32, 'bold'), bg="#476A6F", fg="#1F2633")  # Colored photo label
+colorLabel.place(relx=0.778, rely=0.35, anchor=E)
 
-canvas.pack() #Puts everything onto the GUI
-
+canvas.pack()  # Puts everything onto the GUI
 
 root.mainloop()
-
